334d49254b73d4a499f476302c6780ef6e636dca
Sam Yip <syip@deloitte.co.uk>
2019-04-11 17:24:27 +0100

    Update taxi mini project

--- projects/101_reinforcement_learning/OpenAI_taxi_mini_project/agent.py
+++ projects/101_reinforcement_learning/OpenAI_taxi_mini_project/agent.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 class Agent:
 
-    def __init__(self, nA=6):
+    def __init__(self, nA=6, epsilon=None, alpha=None, gamma=None):
         """ Initialize agent.
 
         Params
@@ -12,6 +12,10 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.eps = epsilon or 0.005
+        self.gamma = gamma or 1
+        self.alpha = alpha or 0.01
+        
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -26,7 +30,7 @@ class Agent:
         """
         return np.random.choice(self.nA)
 
-    def step(self, state, action, reward, next_state, done):
+    def step(self, state, action, reward, next_state, done, sarsa=None):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
         Params
@@ -36,5 +40,9 @@ class Agent:
         - reward: last reward received
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
+        - sarsa: determines which SARSA algorithm to use; take value from 0, "max" or "expected" 
+                 corresponding to SARSA(0), Q-Learning & Expected SARSA respectively
         """
+        self.sarsa = sarsa or 0 
         self.Q[state][action] += 1
+
